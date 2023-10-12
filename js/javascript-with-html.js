d518cc0b0632da1d9bfa8a18ed3601835db073ba
Severiano Bautista <severiano.bautista090@gmail.com>
2023-10-12 16:13:59 -0500

    Javascript-with-html lecture

---
+++ js/javascript-with-html.js
@@ -0,0 +1,29 @@
+"use strict"
+
+// let myAge = 5;
+// console.log(myAge)
+
+//alert
+// alert("Hello Lasso")
+
+//confirm
+let confirmed = confirm("Are you sure you're in the correct cohort?")
+console.log(confirmed)
+
+
+
+// prompt
+let userInput = prompt("Please type in your name")
+console.log(`Great to meet you ${userInput}`)
+
+alert(`Good to meet you ${userInput}`)
+
+//example of concatenation
+let myName = "Sev";
+let myAge = 25;
+
+    console.log("hello" + myName + " You're age is " + myAge);
+//template literal
+    console.log(`Hello ${myName} You're name is ${myAge} 'fsdlkfj' `)
+
+console.log("This is my external js file")
