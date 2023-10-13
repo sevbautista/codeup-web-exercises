41cb2375e9be22ccc3163220bb5d607089fa0486
Severiano Bautista <severiano.bautista090@gmail.com>
2023-10-13 10:15:15 -0500

    cleaned up some of my code so that it would look better

--- js/js/external.js
+++ js/js/external.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// console.log("Hello from external JavaScript")
-// alert ("Welcome to my Website!")
+console.log("Hello from external JavaScript")
+alert ("Welcome to my Website!")
 
 let userInput = prompt("What's your favorite color?")
 console.log(`Great, ${userInput} is my favorite color too!`)
@@ -28,21 +28,21 @@ console.log(`Great, ${userInput} is my favorite color too!`)
 
 
 
-// let userInput1 = parseInt (prompt("How many days did you rent the Little Mermaid for?"))
- // alert(`${userInput1}"?"`)
+let userInput1 = parseInt (prompt("How many days did you rent the Little Mermaid for?"))
+ alert(`${userInput1}"?"`)
 
-// let userInput2 = parseInt (prompt ("And how many days did you have Brother Bear?"))
-// alert (`${userInput2}"?"`)
+let userInput2 = parseInt (prompt ("And how many days did you have Brother Bear?"))
+alert (`${userInput2}"?"`)
 
-// let userInput3 = parseInt (prompt ("And so then how many days did you rent Hercules for?"))
-// alert (`${userInput3}"?`)
+let userInput3 = parseInt (prompt ("And so then how many days did you rent Hercules for?"))
+alert (`${userInput3}"?`)
 
-// let pa = 3
-// let cost = (userInput1* pa) + (userInput2 * pa) +(userInput3 * pa)
+let pa = 3
+let cost = (userInput1* pa) + (userInput2 * pa) +(userInput3 * pa)
 
 //console.log((userInput1 + userInput2 + userInput3) * pa)
 //console.log(`So then if they cost $3 a day, you owe ${cost} `)x`
-// alert(`So then if they cost $3 a day, you owe ${cost} `)
+alert(`So then if they cost $3 a day, you owe ${cost} `)
 
 // Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour.
 // Google pays $400, Amazon $380, and Facebook $350.
@@ -50,11 +50,17 @@ console.log(`Great, ${userInput} is my favorite color too!`)
 // You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
 
 let g = prompt ("How much does Google pay you per hour?")
-let ama = prompt ("How much does Amazon pay you per hour?")
-let f = prompt ("And how much does Facebook pay you?")
-let cost = ((g * 6) + (ama * 4) + (f * 10))
+alert (`${g}, right?`)
 
-alert (`"So then if you worked 20 hours, you should have made ${cost}"`)
+let ama = prompt ("How much does Amazon pay you per hour?")
+alert (`${ama}, right?`)
+
+let f = prompt ("And how much does Facebook pay you?")
+alert (`${f}, right?`)
+
+let pay = ((g * 6) + (ama * 4) + (f * 10))
+
+alert (`"So then if you worked 20 hours, you should have made ${pay}"`)
 // additional variables
 // let gHours = 6
 // let amaHours = 4
