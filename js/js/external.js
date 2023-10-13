19311833c74c1584de8ab434ab003e805e1881e1
Severiano Bautista <severiano.bautista090@gmail.com>
2023-10-13 09:55:17 -0500

    finished the second half of exercise 3 in external commenting out previous work

--- js/js/external.js
+++ js/js/external.js
@@ -1,19 +1,17 @@
+"use strict";
+
 // console.log("Hello from external JavaScript")
 // alert ("Welcome to my Website!")
 
-
-// let userInput = prompt("What's your favorite color?")
-// console.log(`Great, ${userInput} is my favorite color too!`)
+let userInput = prompt("What's your favorite color?")
+console.log(`Great, ${userInput} is my favorite color too!`)
 
 //Write some JavaScript code, that is, variables and operators, to describe the following scenarios.
 // Do not worry about the real operations to get the values, the goal of these exercises is to understand how real world conditions can be represented with code.
 // You have rented some movies for your kids:
 // The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it).
 // If price for a movie per day is $3, how much will you have to pay?
-// Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour.
-// Google pays $400, Amazon $380, and Facebook $350.
-// How much will you receive in payment for this week?
-// You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
 
 // let p = 3
 // let little = 3
@@ -30,18 +28,39 @@
 
 
 
-let userInput1 = prompt("How many days did you rent the Little Mermaid for?")
- alert(`${userInput1}"?"`)
+// let userInput1 = parseInt (prompt("How many days did you rent the Little Mermaid for?"))
+ // alert(`${userInput1}"?"`)
 
-let userInput2 = prompt ("And how many days did you have Brother Bear?")
-alert (`${userInput2}"?"`)
+// let userInput2 = parseInt (prompt ("And how many days did you have Brother Bear?"))
+// alert (`${userInput2}"?"`)
 
-let userInput3 = prompt ("And so then how many days did you rent Hercules for?")
-alert (`${userInput3}"?`)
+// let userInput3 = parseInt (prompt ("And so then how many days did you rent Hercules for?"))
+// alert (`${userInput3}"?`)
 
-let pa = 3
-let cost = ((userInput1 + userInput2 + userInput3) * pa)
+// let pa = 3
+// let cost = (userInput1* pa) + (userInput2 * pa) +(userInput3 * pa)
 
 //console.log((userInput1 + userInput2 + userInput3) * pa)
-console.log(`So then if they cost $3 a day, you owe ${cost} `)
+//console.log(`So then if they cost $3 a day, you owe ${cost} `)x`
+// alert(`So then if they cost $3 a day, you owe ${cost} `)
 
+// Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour.
+// Google pays $400, Amazon $380, and Facebook $350.
+// How much will you receive in payment for this week?
+// You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+let g = prompt ("How much does Google pay you per hour?")
+let ama = prompt ("How much does Amazon pay you per hour?")
+let f = prompt ("And how much does Facebook pay you?")
+let cost = ((g * 6) + (ama * 4) + (f * 10))
+
+alert (`"So then if you worked 20 hours, you should have made ${cost}"`)
+// additional variables
+// let gHours = 6
+// let amaHours = 4
+// let fHours = 10
+//
+// let totalPay = (g * gHours) + (ama * amaHours) + (f * fHours)
+// console.log(totalPay)
+// template literal version of the same result
+// console.log(`Your total pay is ${totalPay}`)
--- warm-ups/js/helloWorld.js
+++ warm-ups/js/helloWorld.js
@@ -1,2 +1,5 @@
 let userAge = prompt ("How old are you?")
 alert (`"Wow, ${userAge}, you're old!"`)
+
+//let userAge - parseInt(prompt ("How old are you?")
+//allows a response to be reduced to the number input and recognized as such later
