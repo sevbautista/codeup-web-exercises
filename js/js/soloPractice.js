ef9fdfea795536e844e81b661ba6b342de4551e3
Severiano Bautista <severiano.bautista090@gmail.com>
2023-10-16 09:31:16 -0500

    warmups.js incomplete warmup for monday 16, trouble with toUpperCase

--- js/js/soloPractice.js
+++ js/js/soloPractice.js
@@ -9,3 +9,12 @@ let name = "Codeup", number = 23;
 console.log (name)
 console.log (number)
 
+// let myString = "42";
+// let myNumber =parseInt(myString);
+
+// function myFunction(alpha, beta) {
+//     let output = alpha / beta;
+//     return output;
+}
+
+// console.log(output)
--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -6,3 +6,9 @@ let year = "2003"
 console.log('My name is ' +name + ' and the year that I graduated high school was ' + year)
 
 //this comment
+function capitalizeFirstLetter (str) {
+const str1 = "codeup"
+
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
