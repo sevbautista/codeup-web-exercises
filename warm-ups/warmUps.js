18edb9ca08147ffba6c6b53e4590070b6abfad4f
Severiano Bautista <severiano.bautista090@gmail.com>
2023-10-16 09:42:26 -0500

    corrected my warmup, added notes to what my mistake was and the correction I made

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -6,9 +6,12 @@ let year = "2003"
 console.log('My name is ' +name + ' and the year that I graduated high school was ' + year)
 
 //this comment
-function capitalizeFirstLetter (str) {
-const str1 = "codeup"
 
+// Warmup Monday 16
+
+function capitalizeFirstLetter (str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+console.log(capitalizeFirstLetter("codeup"));
 
+//I was forgetting to call the function itself, and not just the arguments, I also didn't parenthesis the string
