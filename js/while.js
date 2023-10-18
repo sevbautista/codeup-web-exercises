790692a1bf5ff9410a2f908ea063bfc5481d9542
Severiano Bautista <severiano.bautista090@gmail.com>
2023-10-18 16:46:59 -0500

    while loop exercises, will review 2nd while loop exercise

--- js/while.js
+++ js/while.js
@@ -6,13 +6,18 @@
 //     console.log(i);
 //     i++
 // }
-//
+
+let num = 1;
+while(num <=32768) {
+    console.log(num = num * 2);
+}
 // let num = 1;
-// while(num <=32768) {
-//     console.log(num = num * 2);
+// while(num <=65536){
+//      num*= 2;
+//      console.log(num)
 // }
 
-// -----
+// -----my work
 
 // This is how you get a random number between 50 and 100
 // let allCones = Math.floor(Math.random() * 50) + 50;
@@ -30,26 +35,35 @@
 
     // This expression will generate a random number between 1 and 5
 
-    // Math.floor(Math.random() * 5) + 1;
-
+    Math.floor(Math.random() * 5) + 1;
+let conesBought = 25
 let allCones = 100
-do { conesBought = allCones -= 10;
-    // Math.floor(Math.random() * 5) + 1;
+
+do {
     if (conesBought < allCones) {
-        console.log(`${conesBought} cones sold`)
-} else {
-        if(conesBought > allCones) {
-            console.log(`Sorry, I cannot sell ${conesBought} cones, I only have`)
+        conesBought = Math.floor(Math.random() * 5) + 1;
+    } else {
+        if(allCones === 0) {
+            console.log(`Sorry, I cannot sell ${conesBought} cones, I only have ${allCones} left`)
         }
     }
-} while (allCones >= conesBought)
-    //     if (guess < secretNumber) {
-    //         console.log("Try a higher number.")
-    //     } else {
-    //         console.log("Try a lower number.");
-    //     }
-    //     console.log("Wrong guess, my guy! " + attempt + " total attempts")
-    // }
+} while (  allCones - 25)
 
-// }while (guess !== secretNumber)
-//while is only for condition, do is for logic
+
+//---------Faruuk's walkthrough
+
+//ice cream
+// This is how you get a random number between 50 and 100
+let allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    let soldCones = Math.floor(Math.random() * 5) +1;
+    console.log(allCones)
+    if (soldCones <=allCones) {
+        console.log(`${soldCones} cones sold...`)
+        allCones -= soldCones
+    } else {
+        console.log(`Cannot sell you ${soldCones} cones. I only have ${allCones}`)
+    }
+} while (allCones !== 0)
+console.log("Yay! I sold them all!")
