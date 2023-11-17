8114972986b8de89c6dbbd4e0b55f94680fcfafe
Severiano Bautista <severiano.bautista090@gmail.com>
2023-11-17 13:52:47 -0600

    to-do-list exercise and walkthrough

--- js/adding-and-removing.js
+++ js/adding-and-removing.js
@@ -1,3 +1,5 @@
+// STORING ID TO DECLUTTER CODE AND NOT HAVE TO TYPE EVERYTHING OUT, JUST THE VARIABLE
+const newContent = document.querySelector("#newContent");
 // CREATING AN ELEMENT
 const myNewP = document.createElement("p");
 myNewP.innerText = "I'm a new paragraph.";
@@ -13,7 +15,7 @@ myOtherP.innerText = "I'm a paragraph too!";
 myOtherP.classList.add("text-center");
 document.querySelector("#newContent").appendChild(myOtherP);
 
-// REMOVE AN ELEMENT BY USING THE .REMOVECHILD METHOD. WHICH YOU NEED TO SPECIFY A PARENT ELEMENT AND THE CHILD ELEMENT YOU WANT TO REMOVE.
+// REMOVE AN ELEMENT BY USING THE .removeChild METHOD. WHICH YOU NEED TO SPECIFY A PARENT ELEMENT AND THE CHILD ELEMENT YOU WANT TO REMOVE.
 
 document.querySelector("#newContent").removeChild(myOtherP);
 
@@ -35,7 +37,7 @@ anotherP.id = "anotherP";
 anotherP.classList.add("fs-1", "fw-bold", "text-center");
 document.querySelector("h6").appendChild(anotherP)
 
-const three =document.querySelector('#three');
+const three = document.querySelector('#three');
 const twoAndAHalf = document.createElement("li");
 twoAndAHalf.innerText = "Two and a Half";
 three.before(twoAndAHalf);
@@ -43,3 +45,49 @@ three.before(twoAndAHalf);
 const threeAndAHalf = document.createElement("li");
 threeAndAHalf.innerText = "Three and a half";
 three.after(threeAndAHalf);
+
+document.querySelector("#numbers").innerHTML += `<li>Six</li>`;
+
+
+const toDoInput = document.querySelector("#to-do");
+const toDoForm = toDoInput.parentElement;
+
+// const handleUserInput = () => {
+//     console.log(toDoInput.value);
+// }
+// NEED TO PREVENT BROWSERS DEFAULT ACTION
+const handleUserInput = (event) => {
+    event.preventDefault();
+    console.log(toDoInput.value);
+    toDoInput.value = '';
+}
+toDoForm.addEventListener('submit', handleUserInput);
+
+// OBTAIN REFERENCE TO THE BUTTON THAT WILL TRIGGER DIV CREATION
+const createButton = document.querySelector("#create-a-div");
+// FUNCTION TO CREATE NEW DIVS
+// WITHIN EACH DIV THERE IS A BUTTON WITH THE CLASS 'event-trigger'
+const createDiv = () => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("dynamic-div");
+    newDiv.innerHTML = "<p>I'm a new div!</p>" +
+        "<button class='event-trigger'>Click Me!</button>";
+    document.querySelector("#new-div-section").appendChild(newDiv);
+//     ADD EVENT HANDLER TO BUTTON HERE
+    newDiv.lastChild.addEventListener('click', () => console.log('Clicked'));
+}
+// ADD THE CLICK HANDLER TO THE BUTTON
+createButton.addEventListener('click', createDiv);
+
+// THIS CODE WILL NOT WORK WITHOUT THE HANDLER ABOVE
+const eventButtons = document.querySelectorAll(".event-trigger");
+const buttonEvent = () => console.log('Clicked!');
+eventButtons.forEach(button => button.addEventListener('click', buttonEvent));
+
+const newDivSection = document.querySelector("#new-div-section");
+// THIS CODE WILL LOOK FOR EVENT TRIGGER TO CONSOLE LOG IT
+newDivSection.addEventListener('click', (e)=> {
+    if (e.target.classList.contains('event-trigger')){
+        console.log('I also say Clicked!');
+    }
+})
