7889717bf6fc62e571b1e81f87520b99eecd9a2a
Severiano Bautista <severiano.bautista090@gmail.com>
2023-11-17 09:37:24 -0600

    adding-and-removing lecture continued

--- js/adding-and-removing.js
+++ js/adding-and-removing.js
@@ -1,6 +1,6 @@
 // CREATING AN ELEMENT
 const myNewP = document.createElement("p");
-myNewP.innerText = "I'm a paragraph.";
+myNewP.innerText = "I'm a new paragraph.";
 myNewP.id = "myNewP"
 myNewP.classList.add("fs-2", 'fw-bold', "text-center")
 
@@ -27,3 +27,19 @@ myAwesomeP.classList.add("fs-1", "text-center");
 // document.querySelector("#newContent").appendChild(myAwesomeP)
 
 document.querySelector("#newContent").replaceChild(myAwesomeP, myNewP)
+
+
+const anotherP = document.createElement("h3")
+anotherP.innerText = "Just putting some words here for no reason.";
+anotherP.id = "anotherP";
+anotherP.classList.add("fs-1", "fw-bold", "text-center");
+document.querySelector("h6").appendChild(anotherP)
+
+const three =document.querySelector('#three');
+const twoAndAHalf = document.createElement("li");
+twoAndAHalf.innerText = "Two and a Half";
+three.before(twoAndAHalf);
+
+const threeAndAHalf = document.createElement("li");
+threeAndAHalf.innerText = "Three and a half";
+three.after(threeAndAHalf);
