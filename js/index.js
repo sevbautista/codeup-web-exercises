dfadccdc50a1f5b22e8623b7d4b4893faabc078b
Severiano Bautista <severiano.bautista090@gmail.com>
2023-11-27 15:57:25 -0600

    http requests lec

---
+++ js/index.js
@@ -0,0 +1,7 @@
+fetch(url, {headers: {'Authorization': 'token' + GITHUB_API_KEY}})
+
+    // fetch("/some-url", {
+    // method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    // body: JSON.stringify(someData),
+    // });
